e11976175359d50f9a3eec768badd68cf23f3d7a
Gregoire Clement <gregoire.clement@epfl.ch>
2020-05-01 01:40:52 +0200

    refactoring auth

--- functions/util/firebase_auth.js
+++ functions/util/firebase_auth.js
@@ -4,48 +4,43 @@ const bearerStr = 'Bearer ';
 
 exports.adminID = 'JUSOsVzuneNl6ul26tD2k66mFpD2';
 
-exports.FBAuth = (req, res, next) => {
+exports.checkIsLogged = (req) => {
     let token;
     const auth = req.headers.authorization;
     if (auth && auth.startsWith(bearerStr)) {
         token = auth.replace(bearerStr, '');
     } else {
-        console.error('no token found');
-        return res.status(403).json({error: 'unauthorized request'})
+        return Promise.reject({error: 'unauthorized request'});
     }
+    return admin.auth()
+        .verifyIdToken(token)
+        .then(decodedIDToken => {
+            req.user = decodedIDToken;
+        })
+}
 
-    admin.auth().verifyIdToken(token)
-        .then(decodedToken => {
-            req.user = decodedToken;
+exports.FBAuth = (req, res, next) => {
+    return exports.checkIsLogged(req)
+        .then(() => {
             return next();
         })
         .catch((err) => {
-            console.error('error while verifying error', err);
-            return res.status(403).json(err)
-        })
+            console.error(err);
+            return res.status(403).json(err);
+        });
 };
 
-exports.AdminAuth = (req, res, next) => {
-    let token;
-    const auth = req.headers.authorization;
-    if (auth && auth.startsWith(bearerStr)) {
-        token = auth.replace(bearerStr, '');
-    } else {
-        console.error('no token found');
-        return res.status(403).json({error: 'unauthorized request'})
-    }
+exports.isAdmin = (req) => req.user && req.user.uid === exports.adminID
 
-    admin.auth().verifyIdToken(token)
-        .then(decodedToken => {
-            req.user = decodedToken;
-            if (req.user.uid === exports.adminID) {
+exports.AdminAuth = (req, res, next) => {
+    return exports.checkIsLogged(req)
+        .then(() => {
+            if (exports.isAdmin(req)) {
                 return next();
             } else {
-                return Promise.reject({error: 'user is not an admin'})
+                const uid = req.user ? req.user.uid : 'null'
+                console.error(`user (with uid: ${uid}) is not an admin`);
+                return res.status(403).json({error: 'user is not an admin'});
             }
-        })
-        .catch((err) => {
-            console.error('error while verifying error', err);
-            return res.status(403).json(err)
         })
 };
